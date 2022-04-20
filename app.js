a5ad2b7c58a50cfc8a83a56a80e53d98e8929334
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-04-20 15:17:41 +0200

    object create

--- app.js
+++ app.js
@@ -1,23 +1,141 @@
 let collection = [{
 
-    music: {
+    
 
-        artist: ('Ninho','Cousin Stizz', 'Comethazine'),
-        year: ('2016', '2022'),
-        genre: ('Rap FR', 'Rap US'),
-        producer:('')
-
-        
-
-    },
-
-    movie: {
-
-        title: ('Ninho','Cousin Stizz', 'Comethazine'),
-        year: ('2016', '2022'),
-        genre: ('Rap FR', 'Rap US'),
-        producer: ('')
+        author: 'Ninho',
+        title: 'Binks To Binks 1',
+        year: '2016',
+        genre: 'Rap',
+        language: 'FR',
+        producer: 'LVDR',
+        type: 'Music'
+},
 
 
-    }
-}]
+                {
+    
+
+        author: 'Cousin Stizz',
+        title: 'LBS',
+        year: '2022',
+        genre: 'Rap',
+        language: 'EN',
+        producer: 'Halfway, Michael Seven & Snapz',
+        type: 'Music'
+
+
+
+},
+{
+    
+
+    author: 'Comethazine',
+    title: 'Like Dat',
+    year: '2022',
+    genre: 'Rap',
+    language: 'EN',
+    producer: 'Baby Plugg, BabyPluto, Beatsbyjuko & Comethazine',
+    type: 'Music'
+
+
+
+},
+{
+    
+
+    author: 'Patrick Kack-Brice',
+    title: 'Killer Game',
+    year: '2021',
+    genre: 'Horror/Thriller',
+    language: 'EN',
+    type: 'Movie'
+
+
+
+},
+{
+    
+
+    author: 'Antoine Fugua',
+    title: 'Equalizer',
+    year: '2014',
+    genre: 'Action/Thriller',
+    language: 'EN',
+    type: 'Movie'
+
+
+
+},
+{
+    
+
+    author: 'Martin Scorsese',
+    title: 'Shutter Island',
+    year: '2014',
+    genre: 'Psycho/Thriller',
+    language: 'EN',
+    type: 'Movie'
+
+
+
+},
+{
+    
+
+    author: 'Tony Ayres',
+    title: 'Clickbait',
+    year: '2021',
+    genre: 'Action/Thriller',
+    language: 'EN',
+    type: 'Series'
+
+
+
+},
+{
+    
+
+    author: 'David E. Kelley & Melissa James Gibson',
+    title: 'Anatomy Of A Scandal',
+    year: '2022',
+    genre: 'Action/Thriller',
+    language: 'EN',
+    type: 'Series'
+
+
+
+},
+{
+    
+
+    author: 'Aaron Martin',
+    title: 'Slasher',
+    year: '2016',
+    genre: 'Horror/Thriller',
+    language: 'EN',
+    type: 'Series'
+
+
+
+},
+{
+    
+
+    author: 'Raven Software & Infinity Ward',
+    title: 'Call of Duty Warzone',
+    year: '2020',
+    genre: 'FPS',
+    language: 'EN',
+    type: 'Game'
+
+
+
+},
+
+
+]
+
+for (const item of collection){
+    console.log(item)
+    
+}
