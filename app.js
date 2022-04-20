d4f8f9b82c95a7b0b962e9cfd50810a9756a79d7
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-04-20 14:50:55 +0200

    F.A.I.L Power never stop

---
+++ app.js
@@ -0,0 +1,23 @@
+let collection = [{
+
+    music: {
+
+        artist: ('Ninho','Cousin Stizz', 'Comethazine'),
+        year: ('2016', '2022'),
+        genre: ('Rap FR', 'Rap US'),
+        producer:('')
+
+        
+
+    },
+
+    movie: {
+
+        title: ('Ninho','Cousin Stizz', 'Comethazine'),
+        year: ('2016', '2022'),
+        genre: ('Rap FR', 'Rap US'),
+        producer: ('')
+
+
+    }
+}]
